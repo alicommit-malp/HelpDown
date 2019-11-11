03653c95ebd32603b25132ba49674eb711a6f971
Ali <ali.tabryzy@gmail.com>
2019-11-11 16:36:20 +0300

    relative path for images has been added

---
+++ src/HelpDown/HelpDown.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Globalization;
+using System.IO;
+using HtmlAgilityPack;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Westwind.AspNetCore.Markdown;
+
+namespace HelpDown
+{
+    public static class HelpDown
+    {
+        /// <summary>
+        /// Determine whether the help file for this controller/action exist or not
+        /// </summary>
+        /// <param name="viewContext"></param>
+        /// <param name="cultureInfo"></param>
+        /// <returns>True if the help exist</returns>
+        public static bool Exists(ViewContext viewContext, CultureInfo cultureInfo)
+        {
+            var controllerName = viewContext.RouteData.Values["controller"].ToString();
+            var actionName = viewContext.RouteData.Values["action"].ToString();
+            var lang = cultureInfo.TwoLetterISOLanguageName.ToLower();
+
+            var helpPath = new HelpPath(controllerName, actionName, lang);
+
+            return helpPath.GetPath() != null;
+        }
+
+
+        /// <summary>
+        /// Find the md file by using the controller,action,language of the requester page
+        /// </summary>
+        /// <param name="viewContext"></param>
+        /// <param name="cultureInfo"></param>
+        /// <returns></returns>
+        public static string GenerateHtml(ViewContext viewContext, CultureInfo cultureInfo)
+        {
+            var controllerName = viewContext.RouteData.Values["controller"].ToString();
+            var actionName = viewContext.RouteData.Values["action"].ToString();
+            var lang = cultureInfo.TwoLetterISOLanguageName.ToLower();
+
+            return GenerateHtml(new HelpPath(controllerName, actionName, lang));
+        }
+
+
+        private static string GenerateHtml(HelpPath helpPath)
+        {
+            var html = new HtmlDocument();
+
+            if (helpPath.GetPath() == null) return null;
+            
+            var htmlRaw = Markdown.ParseFromFile(helpPath.GetPath());
+
+            if (string.IsNullOrEmpty(htmlRaw)) return null;
+
+            html.LoadHtml(htmlRaw);
+            foreach (var node in html.DocumentNode.SelectNodes("//img"))
+            {
+                var src = node.Attributes["src"].Value;
+                node.SetAttributeValue("src", ConvertPathIfNeeded(src, helpPath));
+            }
+
+            var stream = new MemoryStream();
+            var streamR = new StreamReader(stream);
+
+            html.Save(stream);
+            stream.Seek(0, SeekOrigin.Begin);
+            return streamR.ReadToEnd();
+        }
+
+
+        private static string ConvertPathIfNeeded(string src, HelpPath helpPath)
+        {
+            try
+            {
+                var url = new Uri(src);
+                return url.ToString();
+            }
+            catch (Exception)
+            {
+                //ignore
+            }
+
+
+            return
+                $"{ApplicationExtension.HelpDownFolderName}/{helpPath.GetControllerName()}/{helpPath.GetActionName()}/{src}";
+        }
+    }
+}
---
+++ src/HelpDown/HelpPath.cs
@@ -0,0 +1,55 @@
+using System.IO;
+
+namespace HelpDown
+{
+    internal class HelpPath
+    {
+        private readonly string _controllerName;
+        private readonly string _actionName;
+        private readonly string _cultureName;
+
+        public HelpPath(string controllerName, string actionName, string cultureName)
+        {
+            _controllerName = controllerName;
+            _actionName = actionName;
+            _cultureName = cultureName;
+        }
+
+
+        /// <summary>
+        /// Try to get the MD file path , if the related culture path does not exist
+        /// therefore it will try to get the default language's MD file
+        /// if the default md file does not exits also , then it will return null
+        /// </summary>
+        /// <returns></returns>
+        public string GetPath()
+        {
+            if (File.Exists(GetAbsolutePath()))
+                return Path.Combine(ApplicationExtension.HelpDownFolderName, _controllerName, _actionName,
+                    $"{_cultureName}.md");
+
+
+            if (File.Exists(GetAbsolutePath(true)))
+                return Path.Combine(ApplicationExtension.HelpDownRootPath, _controllerName, _actionName,
+                    $"{ApplicationExtension.DefaultLanguage.ToLower()}.md");
+
+            return null;
+        }
+
+        private string GetAbsolutePath(bool defaultMdFile = false)
+        {
+            return Path.Combine(ApplicationExtension.HelpDownRootPath, _controllerName, _actionName,
+                !defaultMdFile ? $"{_cultureName}.md" : $"{ApplicationExtension.DefaultLanguage.ToLower()}.md");
+        }
+
+        public string GetControllerName()
+        {
+            return _controllerName;
+        }
+
+        public string GetActionName()
+        {
+            return _actionName;
+        }
+    }
+}
